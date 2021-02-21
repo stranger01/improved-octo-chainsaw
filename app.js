bdf3fdeab7e798f0b877b897cdf83e78e0368443
Stranger01 <joseantoniosysnet@gmail.com>
2021-02-21 18:26:21 -0500

    javascript

---
+++ app.js
@@ -0,0 +1,21 @@
+//estoy aprendiendo Javascript para poder aprender React Native
+
+
+
+var nombre_usuario = "Antonio";
+console.log(nombre_usuario);
+
+let apellido = "Perez";
+
+ apellido = "Martinez"
+console.log(apellido);
+
+const PI = 3.1416;
+console.log(PI);
+
+/**
+ *Un comentario multinelea  
+ * todo esto es un comentarion agregado aca
+ */
+
+
